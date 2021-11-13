1feec58c5cd1cdce3e8b9e9f3436efa13449f622
yogiismntry <yogisumantrii351@gmail.com>
2021-11-13 16:00:10 +0700

    Menambahkan Tugas Pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Yogi</li>
+        <li>Nama Belakang : Sumantri</li>
+</ul>
+<br>
+<h4>Alamat</h4>
+<ul type="none">
+    <li> Bogor</li>
+</ul>
+
+<h4>Tempat Lahir</h4>
+<ul type="none">
+    <li>Bogor</li>
+</ul>
+
+<h4>Olah Raga Favorit</h4>
+<ul type="square">
+    <li>Futsal</li>
+    <li>Bulutangkis</li>
+</ul>
+</section>
